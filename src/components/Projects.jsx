21f5e59897a99da0593f67727620b0715b1519ca
nishantxrana <nishantrana249000@gmail.com>
2024-06-25 18:17:47 +0530

    fixed mobile layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,14 +23,16 @@ function Projects() {
                 </a>
               </h6>
               <p className=" mb-4 text-neutral-400">{project.description}</p>
+              <div className="flex flex-wrap">
               {project.technologies.map((tech, index) => (
                 <span
-                  className=" mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900"
+                  className=" mr-2 mb-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
                   key={index}
                 >
                   {tech}
                 </span>
               ))}
+              </div>
             </div>
           </div>
         ))}
